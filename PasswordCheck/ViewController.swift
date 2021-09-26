bf2891370aff4f05984ce3782a6114c0c01d22f5
hessayon <imaggi99@mail.ru>
2021-09-26 13:08:56 +0300

    Initial Commit

---
+++ PasswordCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasswordCheck
+//
+//  Created by Margo Naumenko on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
