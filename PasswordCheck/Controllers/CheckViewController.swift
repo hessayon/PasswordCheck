5b8429c0fe007204297f3d49f2fbd2d9941ce210
hessayon <imaggi99@mail.ru>
2021-11-04 23:26:14 +0300

    Implement search for password in groups

--- PasswordCheck/Controllers/CheckViewController.swift
+++ PasswordCheck/Controllers/CheckViewController.swift
@@ -6,18 +6,49 @@
 //
 
 import UIKit
+import RealmSwift
 
 class CheckViewController: UIViewController {
+    
     @IBOutlet weak var groupPicker: UIPickerView!
-    private let pickerDataSource = ["None","All groups","Group 1", "Group 2", "Group 3", "Group 4" ]
+    @IBOutlet weak var passwordTextfield: UITextField!
+
+    let realm = try! Realm()
+    private var groups: Results<Group>?
+    // passwords where we are serching for input password
+    private var passwords = [String]()
+    private var pickerDataSource = ["None", "All groups"]
     override func viewDidLoad() {
         super.viewDidLoad()
         groupPicker.dataSource = self
         groupPicker.delegate = self
+        groups = realm.objects(Group.self)
+        if let loadedGroups = groups {
+            for group in loadedGroups {
+                pickerDataSource.append(group.name)
+            }
+        }
+    }
+    
+    @IBAction func checkButtonPressed(_ sender: UIButton) {
+        if let password = passwordTextfield.text {
+            if passwords.firstIndex(of: password) != nil {
+                let alert = UIAlertController(title: "The password is easy :(", message: "", preferredStyle: .alert)
+                let actionOK = UIAlertAction(title: "OK", style: .default) { (action) in
+                }
+                alert.addAction(actionOK)
+                present(alert, animated: true, completion: nil)
+            } else {
+                let alert = UIAlertController(title: "The password is safe :)", message: "", preferredStyle: .alert)
+                let actionOK = UIAlertAction(title: "OK", style: .default) { (action) in
+                }
+                alert.addAction(actionOK)
+                present(alert, animated: true, completion: nil)
+            }
+        }
         
     }
-
-
+    
 }
 
 extension CheckViewController: UIPickerViewDataSource, UIPickerViewDelegate {
@@ -33,5 +64,29 @@ extension CheckViewController: UIPickerViewDataSource, UIPickerViewDelegate {
         return pickerDataSource[row]
     }
     
-    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        passwords = []
+        switch pickerDataSource[row] {
+        case "None":
+            return
+        case "All groups":
+            if let loadedGroups = groups {
+                for group in loadedGroups {
+                    for password in group.passwords {
+                        passwords.append(password.title)
+                    }
+                }
+            }
+        default:
+            if let loadedGroups = groups {
+                let chosenGroups = loadedGroups.filter("name == %@", pickerDataSource[row])
+                for group in chosenGroups {
+                    for password in group.passwords {
+                        passwords.append(password.title)
+                    }
+                }
+            }
+        }
+        print(passwords)
+    }
 }
